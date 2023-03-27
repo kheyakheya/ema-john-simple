cf5ec2e8cf9d64c3f6a7546e64dda1c7ed5e38da
kheyakheya <lubabaazad17@gmail.com>
2023-03-27 20:16:47 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Header from './component/Header/Header'
+import Shop from './component/Shop/Shop'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,8 +10,9 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Shop></Shop>
     </div>
   )
 }
 
-export default App
+export default App;
