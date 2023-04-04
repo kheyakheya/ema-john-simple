190dcf67d9d802feaf46b37c0ca836ba8bdaff32
kheyakheya <lubabaazad17@gmail.com>
2023-04-05 02:32:40 +0600

    module 55 end

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import './Cart.css'
+import React, { Children } from 'react';
+import './Cart.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
-const Cart = ({cart}) => {
+
+const Cart = ({cart,handleClearCart,children}) => {
     // const {cart}=props;
     let total=0;
     let quantity=0;
@@ -27,7 +30,10 @@ const Cart = ({cart}) => {
             <p>Total Shipping: ${totalShipping} </p>
             <p>Tax: ${tax.toFixed(2)} </p>
             <h6>Grand Total:{grandTotal.toFixed(2)} </h6>
+            <button onClick={handleClearCart} className='btn-clear-cart'>  <span>Clear Cart</span> <FontAwesomeIcon  icon={faTrashAlt} /></button>
+            {children}
         </div>
+
     );
 };
 
